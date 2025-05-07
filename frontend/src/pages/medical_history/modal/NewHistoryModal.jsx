63b6c76911030d25652e43291fd8592d10b80f22
Bits8 <bitardos8@gmail.com>
2025-05-07 11:35:54 -0500

    Implementando modulo de historiales medicos

--- frontend/src/pages/medical_history/modal/NewHistoryModal.jsx
+++ frontend/src/pages/medical_history/modal/NewHistoryModal.jsx
@@ -1,10 +1,14 @@
-import { Modal } from "react-bootstrap"
-
-const handleHide = ()=>{
-    
-}
+import { Form, Modal, Row } from "react-bootstrap"
+import { useState } from "react"
 
 const NewHistoryModal = ()=>{
+    const [patients,setPatients] = useState([])
+    const [doctors,setDoctors] = useState([])
+
+    const handleHide = ()=>{
+
+    }
+
     return (
         <Modal>
             <Modal.Header>
@@ -13,7 +17,80 @@ const NewHistoryModal = ()=>{
                 onClick={handleHide}></i>
             </Modal.Header>
             <Modal.Body>
+                <Form>
+                    <Row>
+                        <Form.Group>
+                            <Form.Label className="text-black">Paciente</Form.Label>
+                            <Form.Select
+                            name="patient_id"
+                            defaultValue={""}>
+                                <option value="">Selecciona un paciente</option>
+                                {patients.map((patient)=>{
+                                    return (
+                                        <option key={patient["_id"]} value={patient["_id"]}>
+                                            {patient.user_name} {patient.user_last_name}
+                                        </option>
+                                    )
+                                })}
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
 
+                    <Row>
+                        <Form.Group>
+                            <Form.Label className="text-black">Razón</Form.Label>
+                            <Form.Control
+                            type="text"
+                            name="reason"
+                            >
+                            </Form.Control>
+                        </Form.Group>
+                    </Row>
+
+                    <Row>
+                        <Form.Group>
+                            <Form.Label className="text-black">Diagnostico</Form.Label>
+                            <Form.Control
+                            type="text"
+                            name="diagnosis"
+                            >
+                            </Form.Control>
+                        </Form.Group>
+                    </Row>
+
+                    <Row>
+                        <Form.Group>
+                            <Form.Label className="text-black">Tratamiento</Form.Label>
+                            <Form.Control
+                            type="text"
+                            name="treatment"
+                            >
+                            </Form.Control>
+                        </Form.Group>
+                    </Row>
+
+                    <Row>
+                        <Form.Group>
+                            <Form.Label className="text-black">Médico encargado</Form.Label>
+                            <Form.Select
+                            name="doctor_id"
+                            defaultValue={""}>
+                                <option value="">Selecciona un médico</option>
+                                {doctors.map((doctor)=>{
+                                    return (
+                                        <option key={doctor["_id"]} value={doctor["_id"]}>
+                                            {doctor.worker_name} {doctor.worker_last_name}
+                                        </option>
+                                    )
+                                })}
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
+
+                    <Modal.Footer>
+                        
+                    </Modal.Footer>
+                </Form>
             </Modal.Body>
         </Modal>
     )
