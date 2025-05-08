8f6e3e83937c54a556d1c11b70a4054434271763
ADSO <nibyminson@gmail.com>
2025-05-08 11:30:57 -0500

    Cambios en el menu de la dashboard

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,5 +1,7 @@
 import { Bar } from "react-chartjs-2";
+import { API_URL } from "../../API_URL"
 import '../../assets/css/nice_admin/dashboard.css';
+import { useState,useEffect } from "react"
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,7 +11,6 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -24,24 +25,42 @@ export const HomePage = () => {
     labels: ["Enero", "Febrero", "Marzo", "Abril","Mayo","Junio","Julio","Agosto","Septiembre", "Octubre", "Noviembre", "Diciembre"],
     datasets: [
       {
-        label: "Pacientes atendidos",
-        data: [9, 1, 2, 1],
-        backgroundColor: "#0d6efd",
+        label: "Pacientes atendidos - 2025",
+        data: [6,1,2,1,2,3,1,2,5,2,1,3],
+        backgroundColor: ["#a3e4d7", "#a3d0e4", "#a3b0e4", "#b6a3e4"],
       },
     ],
+  };
+
+  const chartOptionsPaciente = {
+    responsive: true,
+    plugins: {
+      legend: { position: "none" },
+      title: { display: false },
+    },
   };
 
   const medicoData = {
-    labels: ["Dr. Goku", "Dra. Skill Issue"],
+    labels: ["Dr. Goku", "Dra. Skill Issue", "Dr. Schweisteiger"],
     datasets: [
       {
-        label: "Consulta",
-        data: [10, 3],
-        backgroundColor: "#198754",
+        label: "Dr. Goku",
+        data: [14],
+        backgroundColor: ["#bddeb4"],
+      },
+      {
+        label: "Dra. Skill Issue",
+        data: [0,6],
+        backgroundColor: ["#b4ded5"],
+      },
+      {
+        label: "Dr. Schweisteiger",
+        data: [0,0,9],
+        backgroundColor: ["#a3d8b9"],
       },
     ],
   };
-
+  
   const chartOptions = {
     responsive: true,
     plugins: {
@@ -49,6 +68,27 @@ export const HomePage = () => {
       title: { display: false },
     },
   };
+  
+
+  const [item,setItem] = useState({})
+
+  const getItemsAndStock = async()=>{
+    try {
+        const items = await fetch(API_URL + '/dash/all',{credentials:"include"})
+        const itemsJSON = await items.json()
+        setItem(itemsJSON.data)
+        console.log(itemsJSON);
+        
+    } catch (error) {
+        console.error(error)
+    }
+  }
+
+    useEffect(() => {
+      getItemsAndStock()
+  }, [])
+
+
 
   return (
     <main className="main" id="main">
@@ -95,9 +135,9 @@ export const HomePage = () => {
             <i className="bi bi-capsule"></i>
           </div>
           <div className="ps-3">
-            <h6><strong>Acetaminofén</strong></h6>
-            <span className="text-danger small pt-1 fw-bold">7 unidades</span>
-            <span className="text-muted small pt-2 ps-1">nivel crítico</span>
+            <h6><strong>{item.item_name}</strong></h6>
+            <span className={`small pt-1 fw-bold ${item.item_stock < 10 ? 'text-danger ' : 'text-success'}`}>{item.item_stock} unidades</span>
+            <span className="text-muted small pt-2 ps-1">{item.item_stocl < 10 ? 'Inventario Crítico' : 'Inventario Estable' }</span>
           </div>
         </div>
       </div>
@@ -114,7 +154,7 @@ export const HomePage = () => {
               <h5 className="card-title">
                 Pacientes atendidos al mes <span>| Hoy</span>
               </h5>
-              <Bar data={pacientesData} options={chartOptions} />
+              <Bar data={pacientesData} options={chartOptionsPaciente} />
             </div>
           </div>
         </div>
